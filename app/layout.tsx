41a6eff643a114b94be3c12c1990afd52b0cfcfb
ugenethehobo <ugenethehobo@icloud.com>
2026-07-06 21:39:04 -0500

    Minor Tweaks - Service Portal v2 0.0.20

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ import { ThemeProvider } from "@/components/theme-provider";
 import { THEME_STORAGE_KEY } from "@/lib/theme";
 import { getThemeScriptDefault } from "@/lib/theme-server";
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/next"
 
 const inter = Inter({subsets:['latin'],variable:'--font-sans'});
 
